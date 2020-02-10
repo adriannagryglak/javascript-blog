b19d16fec9f721941379e76bbeed51f4af949bf8
Adrianna Gryglak <adrianna.juda@live.com>
2020-02-10 14:03:59 +0100

    Remove class active from links and articles

--- js/script.js
+++ js/script.js
@@ -1,26 +1,36 @@
-
-
 'use strict';
 
-function titleClickHandler(event){
-  console.log('Link was clicked!');
-  console.log(event);
+function titleClickHandler(event) {
+    console.log('Link was clicked!');
+    console.log(event);
 
-  /* remove class 'active' from all article links  */
+    /* remove class 'active' from all article links  */
 
-  /* add class 'active' to the clicked link */
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
-  /* remove class 'active' from all articles */
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
 
-  /* get 'href' attribute from the clicked link */
+    /* add class 'active' to the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+    /* remove class 'active' from all articles */
+    
+    const activeArticles = document.querySelectorAll('.posts article.active');
+    
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }
 
-  /* add class 'active' to the correct article */
+    /* get 'href' attribute from the clicked link */
+
+    /* find the correct article using the selector (value of 'href' attribute) */
+
+    /* add class 'active' to the correct article */
 }
 
 const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
 }
