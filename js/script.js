7e1d1951c12989e1c5d25e759e481f808e1da329
Adrianna Gryglak <adrianna.juda@live.com>
2020-02-25 19:02:08 +0100

    Add js that generate list of tag links in right sidebar

--- js/script.js
+++ js/script.js
@@ -47,7 +47,8 @@ const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles',
   optArticleTagsSelector = '.post-tags .list',
-  optArticleAuthorSelector = '.post-author';
+  optArticleAuthorSelector = '.post-author',
+  optTagsListSelector = '.tags.list';
 
 function generateTitleLinks(customSelector = '') {
 
@@ -82,6 +83,9 @@ generateTitleLinks();
 
 
 function generateTags() {
+
+  /* [NEW] create a new variable allTags with an empty object */
+  let allTags = {};
 
   /* find all articles */
 
@@ -119,6 +123,14 @@ function generateTags() {
 
       html = html + linkHTML;
 
+      /* [NEW] check if this link is NOT already in allTags */
+      if (!allTags.hasOwnProperty(tag)) {
+        /* [NEW] add tag to allTags obcjet */
+        allTags[tag] = 1;
+      } else {
+        allTags[tag]++;
+      }
+
       /* END LOOP: for each tag */
     }
 
@@ -128,7 +140,22 @@ function generateTags() {
 
     /* END LOOP: for every article: */
   }
+  /* [NEW] find list of tags in right column */
+  const tagList = document.querySelector('.tags');
+  /* [NEW] create variable for all links HTML code */
+  let allTagsHTML = '';
+  /*[NEW] START LOOP: for each tag in allTags */
+  for (let tag in allTags) {
+    /*[NEW] generate code of a link and add it to allTagsHTML */
+    
+    allTagsHTML += '<li><a href="#">' + tag + '</a> <span>(' + allTags[tag] + ')</span></li>';
+    
+    /*[NEW] END LOOP: for each tag in allTags*/
+  }
+  /*[NEW] add html from allTagsHTML to tagList */
+  tagList.innerHTML = allTagsHTML;
 }
+
 
 generateTags();
 
